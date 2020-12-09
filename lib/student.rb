5789e2d4ddf261c01048d6909e352cc54e7524c1
Julian <julianrosenthal@yahoo.com>
2020-12-09 13:40:30 -0800

    commit tree 15f6165596582bb71aef6fbbcfeadfb894a52e62

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,37 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ? 
+      LIMIT 1    
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  
   end
   
   def save
@@ -39,5 +59,38 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  # def self.all_students_in_grade_X(grade)
+  #   sql = <<-SQL
+  #     SELECT * 
+  #     FROM *
+  #     WHERE grade = ?
+  #   SQL
+    
+  #   DB[:conn].execute(sql, grade).map do |row|
+  #     self.new 
+  # end
+
+  def self.all_students_in_grade_9 
+    self.all_students_in_grade_X(9)
+  end
+  
+  def self.all_students_in_grade_X(grade_x)
+    self.all.select {|student| student.grade == "#{grade_x}"}
+  end
+
+  def self.first_X_students_in_grade_10(num_students)
+    self.all_students_in_grade_X(10)[0..num_students - 1]
+  end
+
+  def self.first_student_in_grade_10 
+    self.first_X_students_in_grade_10(1)[0]
+  end
+
+  def self.students_below_12th_grade 
+    #Could chain together all students_in_grade_x, but makes more sense to create another select method here 
+    self.all.select {|student| student.grade != "12"}
+  
   end
 end
